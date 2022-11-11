89a92aad21d02e6b84a26e5234e9b03c56c3aac0
jostar-c <2742173382@qq.com>
2022-11-12 03:21:57 +0800

    完成各个页面格式

--- managerpages/pages/7_groupphoto_manage/groupphoto_manage.vue
+++ managerpages/pages/7_groupphoto_manage/groupphoto_manage.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div style="padding: 5px 20px">
     <br />
     <el-form :inline="true" :model="listQuery" class="demo-form-inline">
@@ -19,7 +19,7 @@
         >
       </el-form-item>
     </el-form>
-    <!-- 表格 -->
+    表格
     <el-table
       :data="list"
       element-loading-text="Loading"
@@ -135,183 +135,406 @@
 </template>
    
   <script>
-export default {
-  name: "department",
-  data() {
-    return {
-      /* typecode:this.$route.query.typecode,
-		   typetitle:this.$route.query.typetitle */
-      listLoading: true,
-      list: null,
-      total: 0,
-      listQuery: {
-        page: 1,
-        limit: 10,
-        unit_name: "",
-      },
-      dialogFormVisible: false,
-      dialogStatus: "create",
-      textMap: {
-        update: "修改代收单位",
-        create: "添加代收单位",
-      },
+// export default {
+//   name: "department",
+//   data() {
+//     return {
+//       /* typecode:this.$route.query.typecode,
+// 		   typetitle:this.$route.query.typetitle */
+//       listLoading: true,
+//       list: null,
+//       total: 0,
+//       listQuery: {
+//         page: 1,
+//         limit: 10,
+//         unit_name: "",
+//       },
+//       dialogFormVisible: false,
+//       dialogStatus: "create",
+//       textMap: {
+//         update: "修改代收单位",
+//         create: "添加代收单位",
+//       },
 
-      ruleForm: {
-        id: "",
-        unit_code: "",
-        unit_account: "",
-        unit_name: "",
-        unit_password: "",
-        create_time: "",
-        create_author: "",
-      },
+//       ruleForm: {
+//         id: "",
+//         unit_code: "",
+//         unit_account: "",
+//         unit_name: "",
+//         unit_password: "",
+//         create_time: "",
+//         create_author: "",
+//       },
 
-      rules: {
-        unit_account: [
-          {
-            required: true,
-            message: "请输入代收单位账号",
-            trigger: "blur",
-          },
-        ],
-        unit_name: [
-          {
-            required: true,
-            message: "请输入代收单位名称",
-            trigger: "blur",
-          },
-        ],
-        unit_password: [
-          {
-            required: true,
-            message: "请输入代收单位密码",
-            trigger: "blur",
-          },
-        ],
-      },
-    };
-  },
-  created() {
-    this.axiosdata();
-  },
-  methods: {
-    handleSizeChange(val) {
-      this.listQuery.limit = val;
-      this.axiosdata();
-    },
-    handleCurrentChange(val) {
-      this.listQuery.page = val;
-      this.axiosdata();
-    },
-    onSubmitSelect() {
-      this.axiosdata();
-    },
-    axiosdata() {
-      this.listLoading = true;
-      this.$axios({
-        method: "post",
-        url: "/page/getHs_collection_unit",
-        headers: {
-          token: this.$store.getters.token,
-        },
-        data: this.listQuery,
-      }).then((response) => {
-        this.listLoading = false;
-        this.list = response.data.result.list;
-        this.total = response.data.result.total;
-      });
-    },
+//       rules: {
+//         unit_account: [
+//           {
+//             required: true,
+//             message: "请输入代收单位账号",
+//             trigger: "blur",
+//           },
+//         ],
+//         unit_name: [
+//           {
+//             required: true,
+//             message: "请输入代收单位名称",
+//             trigger: "blur",
+//           },
+//         ],
+//         unit_password: [
+//           {
+//             required: true,
+//             message: "请输入代收单位密码",
+//             trigger: "blur",
+//           },
+//         ],
+//       },
+//     };
+//   },
+//   created() {
+//     this.axiosdata();
+//   },
+//   methods: {
+//     handleSizeChange(val) {
+//       this.listQuery.limit = val;
+//       this.axiosdata();
+//     },
+//     handleCurrentChange(val) {
+//       this.listQuery.page = val;
+//       this.axiosdata();
+//     },
+//     onSubmitSelect() {
+//       this.axiosdata();
+//     },
+//     axiosdata() {
+//       this.listLoading = true;
+//       this.$axios({
+//         method: "post",
+//         url: "/page/getHs_collection_unit",
+//         headers: {
+//           token: this.$store.getters.token,
+//         },
+//         data: this.listQuery,
+//       }).then((response) => {
+//         this.listLoading = false;
+//         this.list = response.data.result.list;
+//         this.total = response.data.result.total;
+//       });
+//     },
 
-    /* 添加表单*/
-    TJCollectionForm() {
-      /* 表单重置*/
-      Object.assign(this.$data.ruleForm, this.$options.data().ruleForm);
+//     /* 添加表单*/
+//     TJCollectionForm() {
+//       /* 表单重置*/
+//       Object.assign(this.$data.ruleForm, this.$options.data().ruleForm);
 
-      this.dialogStatus = "create";
-      this.dialogFormVisible = true;
-    },
-    /* 编辑表单*/
-    BJCollectionForm($index) {
-      //显示修改对话框
-      this.ruleForm = JSON.parse(JSON.stringify(this.list[$index]));
-      //以下效果和上面的不一样
-      //this.ruleForm=this.list[$index]
+//       this.dialogStatus = "create";
+//       this.dialogFormVisible = true;
+//     },
+//     /* 编辑表单*/
+//     BJCollectionForm($index) {
+//       //显示修改对话框
+//       this.ruleForm = JSON.parse(JSON.stringify(this.list[$index]));
+//       //以下效果和上面的不一样
+//       //this.ruleForm=this.list[$index]
 
-      this.dialogStatus = "update";
-      this.dialogFormVisible = true;
-    },
+//       this.dialogStatus = "update";
+//       this.dialogFormVisible = true;
+//     },
 
-    /* 添加按钮*/
-    submitCollectionForm(formname) {
-      this.$refs[formname].validate((valid) => {
-        if (valid) {
-          this.$axios({
-            method: "post",
-            url: "/inserths_collection_unit",
-            data: this.ruleForm,
-          }).then((response) => {
-            if (response.data.resStatus.resCode == 0) {
-              this.axiosdata();
-              this.dialogFormVisible = false;
-              this.$message.success("添加数据成功！");
-            } else {
-              this.$message.error("添加数据失败！");
-            }
-          });
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-    },
-    /* 修改按钮*/
-    updateCollectionForm(formname) {
-      this.$refs[formname].validate((valid) => {
-        if (valid) {
-          this.$axios({
-            method: "post",
-            url: "/updatehs_collection_unit",
-            data: this.ruleForm,
-          }).then((response) => {
-            if (response.data.resStatus.resCode == 0) {
-              this.axiosdata();
-              this.dialogFormVisible = false;
-              this.$message.success("修改数据成功！");
-            } else {
-              this.$message.error("修改数据失败！");
-            }
-          });
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-    },
-    /* 删除按钮*/
-    deleteVisible(scope) {
-      this.$confirm("确定要删除【" + scope.row.unit_name + "】吗？", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
-      }).then(() => {
-        this.$axios({
-          method: "post",
-          url: "/deletehs_collection_unit",
-          data: scope.row,
-        }).then((response) => {
-          if (response.data.resStatus.resCode == 0) {
-            this.axiosdata();
-            this.dialogFormVisible = false;
-            this.$message.error("删除完毕");
-          } else {
-            this.$message.error("删除数据失败！");
-          }
-        });
-      });
-    },
-  },
-};
+//     /* 添加按钮*/
+//     submitCollectionForm(formname) {
+//       this.$refs[formname].validate((valid) => {
+//         if (valid) {
+//           this.$axios({
+//             method: "post",
+//             url: "/inserths_collection_unit",
+//             data: this.ruleForm,
+//           }).then((response) => {
+//             if (response.data.resStatus.resCode == 0) {
+//               this.axiosdata();
+//               this.dialogFormVisible = false;
+//               this.$message.success("添加数据成功！");
+//             } else {
+//               this.$message.error("添加数据失败！");
+//             }
+//           });
+//         } else {
+//           console.log("error submit!!");
+//           return false;
+//         }
+//       });
+//     },
+//     /* 修改按钮*/
+//     updateCollectionForm(formname) {
+//       this.$refs[formname].validate((valid) => {
+//         if (valid) {
+//           this.$axios({
+//             method: "post",
+//             url: "/updatehs_collection_unit",
+//             data: this.ruleForm,
+//           }).then((response) => {
+//             if (response.data.resStatus.resCode == 0) {
+//               this.axiosdata();
+//               this.dialogFormVisible = false;
+//               this.$message.success("修改数据成功！");
+//             } else {
+//               this.$message.error("修改数据失败！");
+//             }
+//           });
+//         } else {
+//           console.log("error submit!!");
+//           return false;
+//         }
+//       });
+//     },
+//     /* 删除按钮*/
+//     deleteVisible(scope) {
+//       this.$confirm("确定要删除【" + scope.row.unit_name + "】吗？", "提示", {
+//         confirmButtonText: "确定",
+//         cancelButtonText: "取消",
+//         type: "warning",
+//       }).then(() => {
+//         this.$axios({
+//           method: "post",
+//           url: "/deletehs_collection_unit",
+//           data: scope.row,
+//         }).then((response) => {
+//           if (response.data.resStatus.resCode == 0) {
+//             this.axiosdata();
+//             this.dialogFormVisible = false;
+//             this.$message.error("删除完毕");
+//           } else {
+//             this.$message.error("删除数据失败！");
+//           }
+//         });
+//       });
+//     },
+//   },
+// };
 </script>
    
   <style>
+</style> -->
+
+
+
+
+
+
+
+
+
+
+
+
+<template>
+  <div>
+    <!-- 2.banner部分start -->
+    <div class="banner">
+      <!-- logo部分 -->
+      <div class="logo">
+        <img src="../../static/logo.png" width="36px" height="auto" />
+      </div>
+      <div>
+        <p>
+          福州大学计算机与大数据学院
+          <br />
+          /软件学院院庆管理系统
+        </p>
+      </div>
+      <div class="out">
+        <router-link to="/">
+          <img src="../../static/out.png" width="24px" height="auto" />
+        </router-link>
+      </div>
+    </div>
+    <div class="w">
+      <!-- 左侧subnav模块 -->
+      <div class="subnav">
+        <ul>
+          <li back>
+            <router-link to="homepage">首页 <span>&gt;</span></router-link>
+          </li>
+          <li>
+            <router-link to="uesr_manage"
+              >用户账号管理 <span>&gt;</span></router-link
+            >
+          </li>
+          <li>
+            <router-link to="comments_manage"
+              >留言评论审核 <span>&gt;</span></router-link
+            >
+          </li>
+          <li>
+            <router-link to="photo_manage"
+              >上传图片审核 <span>&gt;</span></router-link
+            >
+          </li>
+          <!-- <li>
+            <router-link to="groupphoto_manage"
+              >合影模板管理 <span>&gt;</span></router-link
+            >
+          </li> -->
+          <li>
+            <router-link to="class_manage"
+              >班级信息管理 <span>&gt;</span></router-link
+            >
+          </li>
+          <li>
+            <router-link to="map_manage"
+              >地图活动信息更新 <span>&gt;</span></router-link
+            >
+          </li>
+          <li>
+            <router-link to="Live_manage"
+              >直播跳转链接管理 <span>&gt;</span></router-link
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="main">
+        <el-button size="mini" @click="dialogFormVisible = true"
+          >查看</el-button
+        >
+      </div>
+      <!-- 查看照片时候的弹出层 -->
+      <el-dialog
+        title="查看照片"
+        :visible.sync="dialogFormVisible"
+        width="50%"
+        top="10%"
+        center
+      >
+        <span>
+          <img
+            src="../../static/7_groupphoto_manage/gate1.png"
+            style="display: inline-block; height: auto; max-width: 100%"
+          />
+        </span>
+      </el-dialog>
+    </div>
+  </div>
+</template> 
+
+<script>
+export default {
+  data() {
+    return {
+      dialogFormVisible: false, //弹出层默认为关闭
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+.banner {
+  height: 40px;
+  background-color: #90c2de;
+  position: fixed;
+  z-index: 1000;
+  right: 0;
+  bottom: 0;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+}
+.banner p {
+  float: left;
+  color: rgb(61, 145, 213);
+  font-size: 12px;
+  margin-left: -150px;
+  padding-top: 5px;
+}
+.logo {
+  margin-top: 2px;
+  margin-left: 10px;
+  float: left;
+  width: 198px;
+  height: 42px;
+}
+.out {
+  margin-top: 9px;
+  margin-right: -18px;
+  float: right;
+  width: 50px;
+  height: 100px;
+}
+.w {
+  width: 1600px;
+  margin: auto;
+}
+
+/*边栏*/
+.subnav {
+  margin-top: 30px;
+  float: left;
+  width: 165px;
+  height: 100%;
+  background-color: #090f57e4;
+  position: fixed;
+}
+.subnav ul li {
+  text-align: center;
+  font-size: 24px;
+  height: 42px;
+  line-height: 60px;
+  padding-left: 15px;
+  padding-right: 10px;
+}
+.subnav ul li a {
+  font-size: 14px;
+  color: #fff;
+}
+.subnav ul li a:hover {
+  color: #00a4ff;
+}
+body {
+  background-color: #d3dae1;
+  height: 800px;
+  position: relative;
+}
+li {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+}
+.header {
+  height: 800px;
+  margin: 30px auto;
+}
+
+/* sidebar 侧边栏区域 */
+.sidebar {
+  width: 450px;
+  height: 175px;
+  background-color: #fff;
+  position: absolute;
+  top: 50px;
+  left: 300px;
+}
+.sidebar ul {
+  padding: 25px;
+}
+.sidebar ul li {
+  margin-bottom: 30px;
+  font-size: 25px;
+}
+.sidebar ul li a:hover {
+  background-color: aqua;
+  color: #00a4ff;
+}
+.main {
+  margin-top: 40px;
+  margin-left: 165px;
+  width: 100%;
+  height: 100%;
+  float: right;
+  position: fixed;
+}
 </style>
--- userpages/pages/1_homepage/homepage.vue
+++ userpages/pages/1_homepage/homepage.vue
@@ -1,29 +1,255 @@
 <template>
-
-    <!-- test -->
-    <div>
-        <router-link to="login" target="_blank">
-            <button>login</button>
+  <div>
+    <!-- 1.top部分start -->
+    <div class="top">
+      <div class="fzulink">
+        <a href="https://ccds.fzu.edu.cn/index.htm" target="_blank">
+          福州大学计算机与大数据学院/软件学院
+        </a>
+      </div>
+      <div class="user">
+        <!-- 注意！！！登录与未登录跳转目标页面不同，记得改为判断全局变量islogin后进行不同跳转！！！！！！！！！ -->
+        <router-link to="login">
+          <!-- 注意！！！注册与个人中心图标不同，记得改为判断全局变量islogin后改变不同地址！！！！！！！！！ -->
+          <img
+            src="../../static/user.png"
+            href="#"
+            width="15px"
+            height="auto"
+          />
         </router-link>
-        {{ islogin }}
+      </div>
     </div>
+    <!-- top部分end -->
 
+    <!-- 2.banner部分start -->
+    <div class="banner">
+      <!-- logo部分 -->
+      <div class="logo">
+        <img src="../../static/logo.png" width="80px" height="auto" />
+      </div>
+
+      <div class="fzu">
+        <img src="../../static/fzu.png" alt="" width="170px" height="auto" />
+        <img src="../../static/line.png" alt="" width="auto" height="80px" />
+      </div>
+
+      <div>
+        <p>
+          福州大学计算机与大数据学院40周年
+          <br />
+          /软件学院20周年庆
+        </p>
+      </div>
+    </div>
+    <!-- banner部分end -->
+
+    <!-- 1.头部区域start -->
+    <div class="header w">
+      <!-- 导航栏部分 nav -->
+      <div class="nav">
+        <ul>
+          <li>
+            <router-link to="/"><b>首页</b></router-link>
+          </li>
+          <li>
+            <router-link to="bigevent"><b>大事记</b></router-link>
+          </li>
+          <li>
+            <!-- 注意！！！登录与未登录跳转目标页面不同，记得改为判断全局变量islogin后进行不同跳转！！！！！！！！！ -->
+            <router-link to="comments"><b>校友留言</b></router-link>
+          </li>
+          <li>
+            <!-- 注意！！！登录与未登录跳转目标页面不同，记得改为判断全局变量islogin后进行不同跳转！！！！！！！！！ -->
+            <router-link to="schoolphoto"><b>校园风光</b></router-link>
+          </li>
+          <li>
+            <!-- 注意！！！登录与未登录跳转目标页面不同，记得改为判断全局变量islogin后进行不同跳转！！！！！！！！！ -->
+            <router-link to="groupphoto"><b>虚拟合影</b></router-link>
+          </li>
+        </ul>
+      </div>
+      <!-- 搜索模块 -->
+      <div class="search">
+        <input type="text" value="输入您要搜索的专业、毕业年份、班级" />
+        <!-- 注意！！！登录与未登录跳转目标页面不同，记得改为判断全局变量islogin后进行不同跳转！！！！！！！！！ -->
+        <router-link to="class_search"><button></button></router-link>
+      </div>
+    </div>
+    <!-- 头部区域end-->
+
+    <!-- 功能主题 -->
+    <div>homepage</div>
+
+    <!-- footer 底部制作区域start -->
+    <div class="footer">
+      <p>
+        学院地址：福州市闽侯县学园路2号福州大学计算机与大数据学院/软件学院
+        <br />
+        版权声明：© 2022 栋感光波. 版权所有. 保留所有权利
+      </p>
+      <!-- footer 底部制作区域end -->
+    </div>
+  </div>
 </template>
-  
+
 <script>
-export default {
-    data () {
-        return {
-            islogin: this.global.islogin
-        }
-    },
-    created () {
-        this.global_func('Hi')
+//1.获取所有元素元素
+var btns = document.getElementsByTagName("button");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].onclick = function () {
+    //2.清空所有默认的样式
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].style.backgroundColor = "";
     }
+    //3.添加颜色
+    this.style.backgroundColor = "red";
+  };
 }
 </script>
-  
-<style>
 
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.logo {
+  float: left;
+  margin-left: 13px;
+  margin-top: -8px;
+}
+
+.fzu {
+  float: left;
+  margin-left: 5px;
+  margin-top: 12px;
+}
+/* top 区域 */
+.top {
+  height: 24px;
+  background-color: #9c5757;
+}
+.top a {
+  margin-left: 5px;
+  margin-top: 0;
+  font-size: 14px;
+  color: #fff;
+}
+.user {
+  float: right;
+  margin-top: -17px;
+  margin-right: 8px;
+}
+
+/* banner 区域 */
+.banner {
+  width: 100%;
+  height: 100px;
+  background-color: #a40404;
+}
+.banner p {
+  float: left;
+  color: rgb(249, 233, 233);
+  font-size: 24px;
+  padding-left: 10px;
+  padding-top: 17px;
+}
+.nav {
+  float: left;
+  margin-left: 60px;
+}
+
+.nav ul li {
+  float: left;
+  margin: 0 35px;
+}
+
+.nav ul li {
+  float: left;
+  margin: 0 15px;
+}
+
+.nav ul li a {
+  display: block;
+  height: 35px;
+  margin-top: -10px;
+  padding-top: -10px;
+  padding-left: 30px;
+  padding-right: 30px;
+  line-height: 20px;
+  font-size: 24px;
+  color: #a40404;
+}
+
+.nav ul li a:hover {
+  transition: 0.2s;
+  border-bottom: 3px solid #a40404;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+body {
+  height: 900px;
+  position: relative;
+}
+
+.header {
+  height: 42px;
+  /* 此处会层叠w里面的margin */
+  margin: 30px auto;
+}
+
+/* search 搜索模块 */
+
+.search {
+  margin-top: -18px;
+  float: left;
+  width: 412px;
+  height: 35px;
+  background-color: rgb(235, 135, 135);
+  margin-left: 60px;
+}
+
+.search input {
+  float: left;
+  width: 345px;
+  height: 35px;
+  border: 1px solid #a40404;
+  border-right: 0;
+  color: #bfbfbf;
+  font-size: 14px;
+  padding-left: 15px;
+}
+
+.search button {
+  float: left;
+  width: 50px;
+  height: 38px;
+  border: 0;
+  background: url(../../static/button.png);
+}
+
+/* footer 模块 */
+
+.footer {
+  height: 40px;
+  background-color: #bfbfbf;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+
+.footer p {
+  font-size: 12px;
+  color: #666;
+  text-align: center;
+  padding-top: 5px;
+}
 </style>
-  
